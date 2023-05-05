aded903587fc6c2687763d758522f812347778e9
gigabait <Workkerbecov@yandex.ru>
2023-05-05 14:02:54 +0300

    исравленные тесты

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,3 +9,18 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1, 2, 3, 4], 0, 3) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], -2) == [3, 4]
+    assert arrs.my_slice([1, 2, 3, 4], 1, -1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], 5) == []
+    assert arrs.my_slice([1, 2, 3], 0, 5) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3], 1, 1) == []
+    assert arrs.my_slice([1, 2, 3], 2, 2) == []
+    assert arrs.my_slice([], -1) == []
+    assert arrs.my_slice([], 0, -1) == []
+    assert arrs.my_slice([1, 2, 3], -1) == [3]
+    assert arrs.my_slice([1, 2, 3], -5) == [1, 2, 3]
+
+
+
